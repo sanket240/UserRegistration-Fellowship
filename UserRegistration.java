a0fc6304ad9cbf5d544a13c0d5351a8d23969476
sanket2498 <sanketdulange24@gmail.com>
2020-12-26 12:20:55 +0530

    Added : Password rule 4 validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -49,7 +49,7 @@ public class UserRegistration
 		}
 		System.out.println("Enter Password:");
 		password=sc.nextLine();
-		if(Pattern.matches("^(?=.*[A-Z])"+"(?=.*[0-9]).{8,32}$",password))
+		if(Pattern.matches("^(?=.*[0-9])"+"(?=.*[a-z])(?=.*[A-Z])"+"(?=.*[@#$%^&+=]).{8,20}$",password))
 		{
 			System.out.println("Valid Password");
 		}
