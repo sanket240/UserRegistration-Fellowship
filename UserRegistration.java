f2943acc4ac79534dbc4fb35ea6499a66f2dfbed
sanket2498 <sanketdulange24@gmail.com>
2020-12-26 11:59:09 +0530

    Added : Last Name Validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,21 +1,31 @@
 import java.util.Scanner;
 import java.util.regex.Pattern;
-public class UserRegistration 
+public class UserRegistration
 {
-	
-	public static void main(String[] args)
-	{
-		String fname;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter Last Name:");
-		fname=sc.nextLine();
-		if(Pattern.matches("^[A-Z]{1}[a-z]*{3,}",fname))
-		{
-			System.out.println("Valid First Name");
-		}
-		else
-		{
-			System.out.println("Invalid First Name");
-		}
-	}
+
+        public static void main(String[] args)
+        {
+                String fname,lname;
+                Scanner sc=new Scanner(System.in);
+                System.out.println("Enter First Name:");
+                fname=sc.nextLine();
+                if(Pattern.matches("^[A-Z]{1}[a-z]*{3,}",fname))
+                {
+                        System.out.println("Valid First Name");
+                }
+                else
+                {
+                        System.out.println("Invalid First Name");
+                }
+                System.out.println("Enter Last Name:");
+                lname=sc.nextLine();
+                if(Pattern.matches("^[A-Z]{1}[a-z]*{3,}",lname))
+                {
+                        System.out.println("Valid Last Name");
+                }
+                else
+                {
+                        System.out.println("Invalid Last Name");
+                }
+        }
 }
